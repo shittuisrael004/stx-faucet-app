db456eb87eab2f72d66b0c1743260baf9ced7dc9
shittuisrael004 <shittuisrael004@gmail.com>
2025-12-28 00:38:44 +0100

    fix: revert

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,8 +1,8 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    turbo: false,
-  },
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  reactCompiler: true,
 };
 
-module.exports = nextConfig;
+export default nextConfig;
